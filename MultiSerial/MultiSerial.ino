6c44babc50ff4674d2de3c629a0b9bfc43d9b042
Warrior87 <buerk1a@cmich.edu>
2020-11-22 14:41:10 -0500

    Update MultiSerial.ino
    added debug block

--- MultiSerial/MultiSerial.ino
+++ MultiSerial/MultiSerial.ino
@@ -19,6 +19,13 @@
   This example code is in the public domain.
 */
 
+#ifdef DEBUG
+  #define DEBUG_PRINT(x)  Serial.print (x)
+  #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTLN(x)
+#endif
 
 void setup() {
   // initialize both serial ports:
